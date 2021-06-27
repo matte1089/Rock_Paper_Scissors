02501637a8fb267ce5f0a65fdf846a3940ad2539
matte1089 <ke.matte@gmail.com>
2021-06-27 14:16:14 -0400

    Mid way point

--- RPS.js
+++ RPS.js
@@ -1,41 +1,84 @@
-const myArray = ['Rock', 'Paper', 'Scissors'];
+const myArray = ['ROCK', 'PAPER', 'SCISSORS'];
 const computerSelection = computerPlay();
-const playerSelection = 'Rock';
-console.log(playRound(playerSelection, computerSelection));
+
+
+let computerScore = 0;
+let playerScore = 0;
+let drawScore = 0;
+
+
 
 
 //when computerPlay is tested it returns one of the three
 
 function computerPlay(){
-    return myArray[Math.floor(Math.random()* myArray.length)];
+  return myArray[Math.floor(Math.random() * myArray.length)];
 }
+
+
+
+
 
 // setting RPS with an array. will return a random in a variable.
 
-function playRound(playerSelection, computerSelection){
-    if (playerSelection === computerSelection){
-    return 'Draw';
-    }
+function playRound(){
+    let playerSelection = prompt().toUpperCase();
+    let computerSelection = computerPlay();
+  if (playerSelection === computerSelection){
+    drawScore++; 
+    return ('Draw - player chose: ' + playerSelection + ' &' + ' computer chose:' + computerSelection);
+  }
 
-      if ( 
-        (playerSelection === 'Rock' && computerSelection === 'Scissors')|| 
-        (playerSelection === 'Scissors' && computerSelection === 'Paper')||
-        (playerSelection === 'Paper' && computerSelection === 'Rock')
-     )
-     {
-       return 'Humanity Wins!';  
-     }
+  else if (
+    (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+    (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+    (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+  ) {
+    playerScore++;
+    return ('Player Wins - player chose: ' + playerSelection + ' &' + ' computer chose:' + computerSelection);
+  }
 
-     else 
-     (
-        (computerSelection === 'Rock' && playerSelection === 'Scissors')||
-        (computerSelection === 'Scissors' && playerSelection === 'Paper')||
-        (computerSelection === 'Paper' && playerSelection === 'Rock')
+  else
+    (
+      (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
+      (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
+      (computerSelection === 'PAPER' && playerSelection === 'ROCK')
     )
-
-
-    {
-        return 'The Machines have won!';
-    }
+  {
+    computerScore++;
+   return ('Computer Wins - player chose: ' + playerSelection + ' &' + ' computer chose:' + computerSelection);
+  }
 }
-     
+
+function game(){
+  console.log(playRound());
+  showScore();
+  console.log(playRound());
+  showScore();
+  console.log(playRound());
+  showScore();
+  console.log(playRound());
+  showScore();
+  console.log(playRound());
+  showScore();
+
+}
+
+const showScore = () =>  {
+  console.log('player score' + playerScore);
+  console.log('computer score' + computerScore);
+  console.log('draw score' + drawScore);
+  
+
+};
+
+game();
+
+function trackWinner(){
+  if (playerScore == 3 || computerScore == 3){
+    return 'Someone won!'
+  }
+}
+  
+
+
